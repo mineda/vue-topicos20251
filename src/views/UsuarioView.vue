1a88b63221218a25a70e32e9685260cb852969e6
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-03-11 10:05:23 -0300

    Página de usuário

---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>{{ nome }}</p>
+    <p><input type="text" :value="nome" /></p>
+    <p><input type="text" id="nome" v-model="nome" /></p>
+    <p><input type="password" id="senha" v-model="senha" /></p>
+    <p v-if="senha.length < 5">Senha muito curta!</p>
+    <div v-else>Senha ok!</div>
+    <button @click="incluir">Incluir</button>
+
+    <table>
+      <thead>
+        <th>Nome</th>
+        <th>Senha</th>
+      </thead>
+      <tbody>
+        <tr v-for="(usuario, index) in usuarios" :key="index">
+          <td>{{ usuario.nome }}</td>
+          <td>{{ usuario.senha }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const nome = ref<string>('teste')
+const senha = ref<string>('')
+
+const usuarios = ref([
+  { nome: 'teste', senha: '123' },
+  { nome: 'teste2', senha: '123' },
+])
+
+function incluir() {
+  usuarios.value.push({ nome: nome.value, senha: senha.value })
+}
+</script>
